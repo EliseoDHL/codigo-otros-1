e708b4b74c8ba6e4121d3764e266cff6a43809e4
Eliseo de Haro <elidharolpz@gmail.com>
2023-06-20 21:14:02 -0600

    Se suben las modificaciones hechas en la revision de codigo de otros, se modificaron las conexiones del HTML con el CSS y el Script, tambien hubo modificaciones en el JS y se anexaron algunos comentarios al igual que en CSS

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,23 @@
-const baseEndpoint = 'https://api.github.com';
+const baseEndpoint = 'https://api.github.com';//Esta es la conexion con la api de github
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+const $n = document.querySelector('.name');//Se corrigio el querySelector, ya que name, no contaba con la indicacion de que era una clase, lo mismo con blog que en vez de tener un . indicando que era una clase, tenia un # indicando un ID
+const $b = document.querySelector('.blog');
 const $l = document.querySelector('.location');
 
 function displayUser(username) {
   $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
+  const data = fetch(`${usersEndpoint}/${username}`); //Se borro el await que estaba detras del fetch
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  $n.textContent = `${data.name}`;//Se cambia la apostrofe por backticks porque las plantillas literales solo se activan con las backtick
+  $b.textContent = `${data.blog}`;
+  $l.textContent = `${data.location}`;
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  n.textContent = `Algo salió mal: ${err}`;
 }
 
-displayUser('stolinski').catch(handleError);
+// displayUser('stolinski').catch(handleError);
+displayUser('stolinski');
